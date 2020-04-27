ca4c6ab7c6cd78ece99876fdfbd379f231c98cf5
Mazel <mazuel.loic@gmail.com>
2020-04-27 13:08:57 +0200

    fix&feat: Logo name & ensure out dir path

--- bin/maz.js
+++ bin/maz.js
@@ -54,41 +54,46 @@ program
   .command('create [app-name]')
   .description('To create Nuxt x Prismic')
   .action(async (appName) => {
-    let emailCredentials
-    const projectInfos = await inquirer.askProjectInformations(appName)
-    const { appEmailing } = projectInfos
-    if (appEmailing === 'mailgun' || appEmailing === 'smtp') {
-      emailCredentials = await inquirer.askClientEmailCredentials(appEmailing)
+    try {
+      let emailCredentials
+      const projectInfos = await inquirer.askProjectInformations(appName)
+      const { appEmailing } = projectInfos
+      if (appEmailing === 'mailgun' || appEmailing === 'smtp') {
+        emailCredentials = await inquirer.askClientEmailCredentials(appEmailing)
+      }
+      const { installer } = await inquirer.askInstaller()
+      const outDir = (appName || projectInfos.appName).toLowerCase()
+      await generate({ outDir })
+      const allinfos =  {
+        ...projectInfos,
+        ...emailCredentials
+      }
+      await replace({ outDir, allinfos })
+      await install({ installer, outDir })
+      const svgHTML = await svg({ appName: outDir, logoColor: projectInfos.appColorPrimary })
+      await addLogo({ outDir, svgHTML })
+      console.log(
+        chalk.bold(`✅ Project created in directory`),
+        chalk.bold.keyword('dodgerblue')(`'${outDir}'`)
+      )
+      console.log()
+      console.log(chalk.bold(`Run project:`))
+      console.log()
+      console.log(chalk(`    cd ${outDir}`))
+      console.log(chalk(`    npm run serve`))
+      console.log()
+      process.exit(0)
+    } catch (err) {
+      console.error(err)
+      process.exit(1)
     }
-    const { installer } = await inquirer.askInstaller()
-    const outDir = (appName || projectInfos.appName).toLowerCase()
-    await generate({ outDir })
-    const allinfos =  {
-      ...projectInfos,
-      ...emailCredentials
-    }
-    await replace({ outDir, allinfos })
-    await install({ installer, outDir })
-    const svgHTML = await svg({ outDir, fillColor: projectInfos.appColorPrimary, strokeColor: projectInfos.appColorSecondary })
-    await addLogo({ outDir, svgHTML })
-    console.log(
-      chalk.bold(`✅ Project created in directory`),
-      chalk.bold.keyword('dodgerblue')(`'${outDir}'`)
-    )
-    console.log()
-    console.log(chalk.bold(`Run project:`))
-    console.log()
-    console.log(chalk(`    cd ${outDir}`))
-    console.log(chalk(`    npm run serve`))
-    console.log()
-    process.exit(0)
   })
 
 program
-  .command('get-svg [app-name] [fill-color] [stroke-color]')
+  .command('get-svg [app-name] [logo-color]')
   .description('Generate SVG with String')
-  .action(async (appName, fillColor, strokeColor) => {
-    const svgHTML = await svg({ appName, fillColor, strokeColor })
+  .action(async (appName, logoColor) => {
+    const svgHTML = await svg({ appName, logoColor })
     console.log(svgHTML)
     process.exit(0)
   })
